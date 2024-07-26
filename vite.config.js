2accfb5aae98d5160b7e522a042cca111e671b05
muhammedjunaid <junaidmuhammedmj@gmail.com>
2024-07-26 14:03:22 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/mirage-web/",
+  plugins: [react()],
+});
 
 // import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/mirage-web/', // This should match the path in your `homepage` field
-  build: {
-    outDir: 'dist', // Ensure this matches your `deploy` script
-  },
-});
+// export default defineConfig({
+//   base: '/mirage-web/', // This should match the path in your `homepage` field
+//   build: {
+//     outDir: 'dist', // Ensure this matches your `deploy` script
+//   },
+// });
